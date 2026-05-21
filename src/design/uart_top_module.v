2a102129ba626a2d98a06384066d29e68d9fa8ed
Moulik-418 <132876160+Moulik-418@users.noreply.github.com>
2026-05-21 11:37:43 +0530

    Update uart_top_module.v

--- src/design/uart_top_module.v
+++ src/design/uart_top_module.v
@@ -9,8 +9,7 @@ output uart_xmit_dataH;
 input uart_REC_dataH;
 wire clk;
 u_baud#(.baud_rate(baud_rate))m1(.sys_clk(sys_clk), .clk(clk), .sys_rst_l(sys_rst_l));
-uart_tx#(.width(width),.baud_rate(baud_rate))m2(.sys_clk(sys_clk), .sys_rst_l(sys_rst_l), .xmitH(xmitH), .xmitdataH(xmitdataH), .uart_xmit_dataH(uart_xmit_dataH), .xmit_doneH(xmit_doneH), .xmit_active(xmit_active));
-uart_rx#(.width(width), .baud_rate(baud_rate))m3(.sys_clk(sys_clk),.sys_rst_l(sys_rst_l), .uart_REC_dataH(uart_REC_dataH), .rec_readyH(rec_readyH), .rec_busy(rec_busy), .rec_dataH(rec_dataH) );
+uart_tx#(.width(width),.baud_rate(baud_rate))m2(.clk(clk), .sys_rst_l(sys_rst_l), .xmitH(xmitH), .xmitdataH(xmitdataH), .uart_xmit_dataH(uart_xmit_dataH), .xmit_doneH(xmit_doneH), .xmit_active(xmit_active));
+uart_rx#(.width(width), .baud_rate(baud_rate))m3(.clk(clk),.sys_rst_l(sys_rst_l), .uart_REC_dataH(uart_REC_dataH), .rec_readyH(rec_readyH), .rec_busy(rec_busy), .rec_dataH(rec_dataH) );
 
 endmodule
-
