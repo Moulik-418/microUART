c02f901f343acd1769227e6f506b1637c3cb6ffd
Moulik-418 <132876160+Moulik-418@users.noreply.github.com>
2026-05-21 15:54:43 +0530

    Update uart_rx.v

--- src/design/uart_rx.v
+++ src/design/uart_rx.v
@@ -5,7 +5,7 @@ module uart_rx#(parameter width = 4'd8, parameter integer baud_rate = 9600)(clk,
 input clk, sys_rst_l, uart_REC_dataH;
 output reg [width-1:0]rec_dataH;
 output reg rec_readyH;
-output rec_busy;
+output reg rec_busy;
 
 wire clk;
 reg [3:0]i;
@@ -14,23 +14,26 @@ reg [1:0]states;
 integer count;
 
 localparam IDLE = 2'd0, REC_DATA = 2'd1, STOP = 2'd2;
-assign rec_busy = (states == REC_DATA || states == STOP)?1'b1 : 1'b0;
+//assign rec_busy = (states == REC_DATA || states == STOP)?1'b1 : 1'b0;
 
   
 always@(posedge clk or negedge sys_rst_l)begin 
 if(!sys_rst_l)begin 
     rec_dataH <= {width{1'b0}};
-    rec_readyH <= 1'b0;
+    rec_readyH <= 1'b1;
     i <= 4'd0;
     tmp_data <= {width{1'b0}};
     states <= 2'd0;
     count <= 0;
+    rec_busy <= 1'b0;
 end
 else begin 
 
 case(states)
     IDLE : begin 
+        rec_busy <= 1'b0;
        rec_dataH <= tmp_data;
+       rec_readyH <= 1'b1;
       if(!uart_REC_dataH)begin 
         if(count >= 7)begin 
                 count <= 0;
@@ -48,7 +51,8 @@ case(states)
     end
     
     REC_DATA : begin 
-
+        rec_readyH <= 1'b0
+       rec_busy <= 1'b1;
       if(count >= 15)begin 
       tmp_data[i] <= uart_REC_dataH;        
           count <= 0;
@@ -68,8 +72,9 @@ case(states)
                 count <= 0;
 
                 if(uart_REC_dataH == 1'b1)begin 
-                     rec_readyH <= 1'b1;    
-                                  rec_dataH <= tmp_data;
+                     rec_readyH <= 1'b1;
+                     rec_busy <= 1'b0;    
+                     rec_dataH <= tmp_data;
 
                 end
                 else rec_readyH <= 1'b0;
